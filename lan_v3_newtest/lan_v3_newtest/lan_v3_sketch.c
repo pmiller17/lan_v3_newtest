0303272c83a840f53b1c0ec6eb3f6f622da6ecc7
Paul Miller <pmiller17@gmail.com>
2014-05-08 21:27:22 -0400

    add lan_v3_sketch
    adds a file for the main source code to operate with LIGHT,
    NEEDS_CHARGE, SAFE_OFF, and CHARGING

---
+++ lan_v3_newtest/lan_v3_newtest/lan_v3_sketch.c
@@ -0,0 +1,113 @@
+/*lan_v3_switch case hierarchy with generic names*/
+
+#include "lan.h"
+#include "lightning.h"
+#include "charging.h"
+#include "
+
+#define TRUE 1
+#define FALSE 0
+
+volatile lantern_mode_t lantern_mode, previous_mode;
+
+
+int main(void)
+{
+	setup();
+	for(;;)
+	{
+		loop();
+	}
+}
+
+void setup(void)
+{
+	/*insert macros and initializations here*/
+}
+
+void loop(void)
+{
+	switch(lantern_op_mode)
+	{
+		case LIGHTING:
+			if(task.debounce_button)
+			{	
+				button_pressed = debounce_button();
+				task.debounce = FALSE;
+			}
+			
+			if(button_pressed)
+			{
+				cycle_lighting();
+				button_pressed = FALSE;
+			}
+			
+			/*need function to say "light LED"?*/
+			lighting_mode_control();	//will sleep if "OFF" and control light otherwise
+			
+			break;
+			
+		case CHARGING:
+			charge_battery();
+			
+			break;
+		
+		case NEEDS_CHARGE:
+			if(task.debounce_button)
+			{
+				button_pressed = debounce_button();
+			}
+			
+			if(button_pressed)
+			{
+				flicker_led();
+				button_pressed = FALSE;
+			}
+			break;
+			
+		case SAFE_OFF:
+			
+			if(task.debounce_jack)
+			{
+				jack_plugged_in = debounce_jack();
+				task.debounce_jack = FALSE;
+			}
+			
+			if(jack_plugged_in)
+			{
+				lantern_mode = CHARGING;
+			}
+			else
+			{
+				lantern_mode = previous_mode;
+				set_up_noncharging(); 
+				/*	re-enable stuff, set light to OFF?
+					button & jack PCI enables.
+				*/
+			}
+	}
+
+}
+
+ISR(PCINT_vect)
+{
+	if(JACK_PLUGGED_IN_NOW)
+	{
+		LED_DISABLE;
+		/*other safety disables*/
+		previous_mode = lantern_mode;
+		lantern_mode = SAFE_OFF;
+		task.debounce_jack = TRUE; 
+	}
+	
+	else if(BUTTON_PRESSED_NOW)
+	{
+		task.debounce_button = TRUE;
+	}
+	
+	else if(JACK_PLUGGED_IN_NOW == FALSE)
+	{
+		previous_mode = lantern_mode;
+		lantern_mode = SAFE_OFF;
+	}
+}
